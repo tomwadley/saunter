fb31e34f65a1527b4df2d37526d5e4426c0b35b7
Tom Wadley <tom@tomwadley.net>
2014-04-21 11:02:56 +0100

    Loading map for external json file and pre-loading images

--- mead/core.js
+++ mead/core.js
@@ -4,8 +4,68 @@ define('mead/core', [
 function ($) {
 
   return {
-    init: function(map, x, y, direction) {
-      setPosition(new Position(x, y, direction), map);
+    init: function(mapUrl) {
+      loadMap(mapUrl, function(init, map) {
+        $(function() {
+          setPosition(init, map);
+        });
+      });
+    }
+  }
+
+  function loadMap(mapUrl, successFunc) {
+    console.log('Loading map: ' + mapUrl);
+    $.get(mapUrl)
+    .success(function(data) {
+      var metadata = parseMetadata(data);
+      var init = parseInit(data);
+      var map = parseCells(data);
+      successFunc(init, map);
+    })
+    .error(function() {
+      alert('Failed to load map "' + mapUrl + '"');
+    });
+
+    function parseMetadata(data) {
+      var metadata = data.metadata || {};
+      console.log('Map name: ' + metadata.name);
+      console.log('Map version: ' + metadata.version);
+      console.log('Map description: ' + metadata.description);
+    }
+
+    function parseInit(data) {
+      var init = data.init || {};
+      if (init.x === undefined) console.log('Error: No init x co-ord defined');
+      if (init.y === undefined) console.log('Error: No init y co-ord defined');
+      if (init.direction == undefined) console.log('Error: No init direction defined');
+      var pos = new Position(init.x, init.y, init.direction);
+      console.log('Init position:', pos);
+      return pos;
+    }
+
+    function parseCells(data) {
+      var map = [];
+      var cells = data.cells || [];
+      console.log('Cells: ' + cells.length);
+      if (cells.length == 0) {
+        console.log('Error: No cells found!');
+      }
+      for (var i = 0; i < cells.length; i++) {
+        var cell = cells[i];
+        if (!map[cell.x]) map[cell.x] = [];
+        if (map[cell.x][cell.y]) {
+          console.log('Warning: Duplicate cell co-ord: ' + cell.x + ', ' + cell.y);
+        }
+        map[cell.x][cell.y] = cell;
+
+        $(function() {
+          if (cell.north) getImage(cell.north);
+          if (cell.east) getImage(cell.east);
+          if (cell.south) getImage(cell.south);
+          if (cell.west) getImage(cell.west);
+        });
+      }
+      return map;
     }
   }
 
@@ -106,13 +166,7 @@ function ($) {
     toggleActive($right, uiState.rightActive);
 
     function setImg(imgName) {
-      var $img = $mainImg.find('.img#img_' + imgName);
-      if ($img.length == 0) {
-        $img = $('<div id="img_' + imgName + '" class="img"></div>');
-        var imgUrl = 'url(' + uiState.img + '.jpg)';
-        $img.css('background-image', imgUrl);
-        $mainImg.append($img);
-      }
+      var $img = getImage(imgName);
       $img.addClass('active');
       $mainImg.find('.img:not(#img_' + imgName + ')').removeClass('active');
     }
@@ -120,6 +174,18 @@ function ($) {
     function toggleActive($elem, active) {
       $elem.toggleClass('active', active);
     }
+  }
+
+  function getImage(imgName) {
+    var $mainImg = $('#mainImg');
+    var $img = $mainImg.find('.img#img_' + imgName);
+    if ($img.length == 0) {
+      $img = $('<div id="img_' + imgName + '" class="img"></div>');
+      var imgUrl = 'url(' + imgName + '.jpg)';
+      $img.css('background-image', imgUrl);
+      $mainImg.append($img);
+    }
+    return $img;
   }
 
 });
--- mead/main.js
+++ mead/main.js
@@ -6,15 +6,7 @@ function ($, core) {
 
   return {
     init: function () {
-      var map = [];
-      map[0] = [];
-      map[0][0] = { north: 'img1', east: 'img1', south: 'img1' };
-      map[1] = [];
-      map[1][0] = { north: 'img1', east: 'img1', south: 'img1', west: 'img1' };
-
-      $(function() {
-        core.init(map, 0, 0, "north");
-      });
+      core.init('map.json');
     }
   };
 
