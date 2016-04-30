be924602464c50eb94a3019827f1f3220ae7a767
Tom Wadley <tom@tomwadley.net>
2016-04-30 12:49:51 +0100

    Remove unused callback

--- saunter/main.js
+++ saunter/main.js
@@ -6,9 +6,7 @@ function ($, core) {
 
   return {
     init: function () {
-      core.init('map.json', [
-        { x: 1, y: 0, direction: "north", func: function(u) { u.leftActive = false; } }
-      ]);
+      core.init('map.json', []);
     }
   };
 
